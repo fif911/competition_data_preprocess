df37e1d8314a04dd7c2ff4895fde2802bb414e5d
Oleksandr Zakotianskyi <ozakotianskyi@pixsystem.com>
2024-05-01 23:34:39 +0200

    Add R script for populating region details; two columns are added: region(with 7 region division) and region23

--- piece_science_R/add_region_to_cm.R
+++ piece_science_R/add_region_to_cm.R
@@ -5,6 +5,7 @@ library(stevemisc) # a dependency, but also used for standardizing variables for
 library(tictoc)
 library(countrycode)
 library(ggplot2)
+library(fastDummies) # added for one-hot encoding
 
 # read cm_features.csv
 cm_features <- read_csv("data/cm_features_v0.6.csv")
@@ -32,8 +33,20 @@ rows_with_na_region23 <- cm_features %>%
 # assert there are no rows with NA region
 stopifnot(nrow(rows_with_na_region) == 0)
 stopifnot(nrow(rows_with_na_region23) == 0)
-# print done
+
+# TODO: do one hot encoding for region and region23 with meaningful name of the columns (region_1, region_2, ...)
+# and for region23 (region23_1, region23_2, ...)
+# cm_features <- cm_features %>%
+#   mutate(across(c(region, region23), ~factor(.))) %>%
+#   pivot_longer(cols = c(region, region23), names_to = "region_type", values_to = "region_value") %>%
+#   mutate(name = paste0(region_type, "_", region_value)) %>%
+#   pivot_wider(names_from = name, values_from = region_value, values_fill = list(region_value = 0)) %>%
+#   mutate(across(starts_with("region"), ~as.integer(!is.na(.))))
+
+# One hot encoding for region and region23, drop the first dummy to avoid multicollinearity
+cm_features <- dummy_cols(cm_features, select_columns = c("region", "region23"), remove_selected_columns = FALSE, remove_first_dummy = TRUE)
+
+
 print("Success")
-# save to csv
 cm_features %>% write_csv("data/cm_features_v0.7.csv")
 print("Saved to data/cm_features_v0.7.csv")
